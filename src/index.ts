9cc68359f8a7701078eb4aedf0a60c27d0fc7914
Kythyria Tieran <kythyria@berigora.net>
2019-02-16 21:17:02 +0000

    Store a bot token in the configuration. That's all discord.js needs.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as deviantart from './deviantart/api';
 import { readFileSync } from 'fs';
+import * as Discord from 'discord.js';
 
 interface ConfigFile {
     dataDirectory: string;
     deviantart: { clientID: string, clientSecret: string };
-    discord: { clientID: string, clientSecret: string };
+    discord: { clientID?: string, clientSecret?: string, botToken: string };
 }
 
 let configfile = process.argv[2];
@@ -12,6 +13,27 @@ let configdata = readFileSync(configfile, "utf8");
 let config : ConfigFile = JSON.parse(configdata);
 
 let da = new deviantart.Api(config.deviantart.clientID, config.deviantart.clientSecret);
+let discord = new Discord.Client();
+
+discord.on("ready", async () => {
+    console.log(`Logged in as ${discord.user.tag}!`);
+    let appinfo = await discord.fetchApplication();
+    console.log(`Join URL: https://discordapp.com/api/oauth2/authorize?client_id=${appinfo.id}&scope=bot&permissions=1`);
+    try {
+        let dmchannel = await appinfo.owner.createDM();
+        await dmchannel.send("Started!");
+    }
+    catch(e) {
+        console.log("Couldn't send DM to owner. Do you have a guild in common?");
+        console.log(e);
+    }
+});
+
+discord.on("disconnect", (evt) => {
+    console.log("Disconnected:", evt);
+})
+
+discord.login(config.discord.botToken);
 
 async function getFirstThing() {
     try {
@@ -29,4 +51,4 @@ async function getFirstThing() {
     }
 }
 
-getFirstThing();
+//getFirstThing();
